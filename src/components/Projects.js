f5b89ba6941219686be025d3ca2ce7a7f52279f1
Ryan Gao <ryantchgao@googlemail.com>
2022-12-24 05:03:32 -0800

    updating project data

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,13 +6,13 @@ import { ExternalLinkIcon } from '@chakra-ui/icons'
 const Projects = ({ title, image, date, description, links, technologies }) => {
     console.log(technologies)
     return (
-        <Flex direction="row" border="1px" borderColor="#AEAEAE" borderRadius="30px" w="800px" marginTop="25px" marginBottom="25px" p="25px">
+        <Flex direction="row" border="1px" borderColor="#AEAEAE" borderRadius="30px" w="800px" m="20px" p="25px" align="center">
             <Image w="auto" maxHeight="200px" borderRadius="30px" src={image} />
             <Flex direction="column" marginLeft="25px">
                 <Text fontSize="20px" fontWeight="semibold"> {title} <Link href={links}> <ExternalLinkIcon paddingBottom="5px" w={7} h={7}/> </Link> </Text>
                 <Text fontSize="17.5px" fontWeight="medium"> {date} </Text>
-                <Text fontSize="17px" lineHeight="23px" paddingTop="15px"> {description} </Text>
-                <Flex justify="row" paddingTop="10px" flexWrap="wrap"> {technologies.map((elem) => ( <Flex border="2px" borderColor="#AEAEAE" borderRadius="12px" p="5px" marginRight="5px"  marginTop="5px"  marginBottom="5px"> {elem} </Flex>))} </Flex>
+                <Text fontSize="16px" lineHeight="23px" paddingTop="15px"> {description} </Text>
+                <Flex justify="row" paddingTop="10px" flexWrap="wrap"> {technologies.map((elem) => ( <Flex border="2px" borderColor="#AEAEAE" borderRadius="12px" p="5px" marginRight="10px"  marginTop="5px"  marginBottom="5px"> {elem} </Flex>))} </Flex>
                 
             </Flex>
         </Flex>
--- src/components/data.js
+++ src/components/data.js
@@ -4,18 +4,49 @@ const data = [
     { 
         title: "Simplif.ai [Finalist @ HackCamp 2022]",
         image: figma,
-        date: "November 2022",
-        description: "AI Powered ",
-        technologies: ["YANG", "Next.js", "YANG", "Next.js", "YANG", "Next.js", "YANG", "Next.js", "YANG", "Next.js"],
+        description: "An AI-powered web app that summarizes text from images using image recognition and NLP. It aims to tackle the issue of accessible language and comprehension for all, particularly those with ADHD or who speak English as a second language. We found that when reading school textbooks or news articles online, a major issue that arises is the unnecessary amount of text that is required to explain an extremely simple concept.",
+        technologies: ["React.js", "Express.js", "Node.js", "Google Cloud Vision", "NLP", "JavaScript", "Figma"],
         links: "https://devpost.com/software/simplif-ai-n4u3sr"
     },
     { 
-        title: "test",
+        title: "Recipe Management System",
         image: figma,
-        date: "November 2022",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-        technologies: ["YANG", "Next.js"] 
-    }
+        description: "GUI-based recipe manager built using Java that allows users to add, update, upvote, save, and load recipes. Built for UBC CPSC 210 2022S.",
+        technologies: ["Java", "Swing", "JUnit"] 
+    },
+    { 
+        title: "Habifix [3rd Place @ Hack the 6ix 2021]",
+        image: figma,
+        description: "A web application that analyzes user habits using video action recognition with the aim of reducing unproductive habits and increasing productive ones.",
+        technologies: ["React.js", "Flask", "TensorFlow", "OpenCV", "Python", "JavaScript", "Chart.js", "Figma"],
+        links: "https://devpost.com/software/habifix"
+    },
+    {
+        title: "This Day in History",
+        image: figma,
+        technologies: ["React.js", "JavaScript", "Firebase"],
+        description: "A web application that calls a Wikipedia API and generates a specific event that occured on a user selected day. Also optimized for mobile display.",
+        links: "https://chatapp-893e3.web.app/"
+    },
+    {
+        title: "Chat App",
+        image: figma,
+        technologies: ["React.js", "JavaScript", "Firebase"],
+        description: "Simple chat app that allows users to send and clear messages. Data persistence implemented using a Firebase Realtime Database.",
+        links: "https://chatapp-893e3.web.app/"
+    },
+    { 
+        title: "A Sound Mood [2nd Place @ XdHacks Mini LA 2020]",
+        image: figma,
+        description: "A Discord bot application that gives song recommendations based on a user's listening habits.",
+        technologies: ["Python", "MongoDB", "Heroku", "Discord.py", "Spotipy"],
+        links: "https://www.youtube.com/watch?v=DTWPGfeU49o"
+    },
+    { 
+        title: "Personal Website",
+        description: "this website!",
+        technologies: ["ChakraUI", "React.js", "JavaScript", "HTML/CSS", "Figma"]
+    },
 
 ]
 
