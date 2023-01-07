589cf9ed32c07df09af0af63771e33b40380a1e2
Ryan Gao <ryantchgao@googlemail.com>
2023-01-07 06:52:50 -0800

    color change

--- src/App.js
+++ src/App.js
@@ -7,13 +7,13 @@ import MainCard from './components/MainCard'
 function App() {
   const { colorMode, toggleColorMode } = useColorMode()
 
-  const border = useColorModeValue("#d1d1d1", "gray.800")
+  const border = useColorModeValue("#d1d1d1", "#3D3D3D")
 
   return (
     <Box overflow="hidden">
       <Box w="100vw" minW="320px" overflow="hidden" borderBottom="1px" borderColor={border}>
         <Flex justify="space-between" align="center" m="25px" overflow="hidden">
-          <Text fontSize="22px">Ryan Gao</Text>
+          <Text fontSize="22px" fontWeight="bold">Ryan Gao</Text>
           <Button onClick={toggleColorMode}>
             Toggle {colorMode === 'light' ? 'Dark' : 'Light'}
           </Button>
